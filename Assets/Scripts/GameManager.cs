2bb1d888af74a3d28ebf9038d1ee65bc86423b11
Roy Snows of Oak <astralenigma@hotmail.com>
2023-04-23 10:38:15 +0100

    Animated phone.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -94,7 +94,7 @@ public class GameManager : MonoBehaviour
 
     private void UpdateCanvas()
     {
-        phone.SetFloat("Notifications",activeTasks.Count);
+        phone.SetInteger("notif", activeTasks.Count);
         //Canvas Update procedure
     }
     #region Energy Functions
