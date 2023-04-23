e74a604d9f37ce9a450636bf2679cc6b747c755a
Roy Snows of Oak <astralenigma@hotmail.com>
2023-04-23 08:34:27 +0100

    Fixed Garbaj and game end.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -148,9 +148,9 @@ public class GameManager : MonoBehaviour
         {
             GameOver();
         }
-        if (time > (startHour * 60 + startMinute) - 24 * 60)
+        if (time > (startHour * 60 + startMinute) + 1440)
         {
-            gameEnded = true;
+            EndGame();
         }
     }
     void GameOver()
@@ -260,6 +260,7 @@ public class GameManager : MonoBehaviour
     internal void RemoveTask(Task task)
     {
         activeTasks.Remove(task);
+        tasksDone++;
     }
 
     internal void AddActiveTask(Task task)
