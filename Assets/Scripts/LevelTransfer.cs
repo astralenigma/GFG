01470f8a5e8008ea7469684f3278f08c74115565
Roy Snows of Oak <astralenigma@hotmail.com>
2023-04-23 01:20:05 +0100

    Level transfer cleaning.

--- Assets/Scripts/LevelTransfer.cs
+++ Assets/Scripts/LevelTransfer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class levelTransfer : MonoBehaviour
+public class LevelTransfer : MonoBehaviour
     
 {
     public Transform destiny;
